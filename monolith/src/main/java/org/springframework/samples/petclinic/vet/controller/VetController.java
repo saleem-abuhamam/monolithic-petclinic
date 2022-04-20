11be4d5f42064201c0672e039db75dfc14f2ca35
axkfk <marc-andre.orthmann@dedalus.eu>
2022-04-20 16:09:38 +0200

    step one

--- monolith/src/main/java/org/springframework/samples/petclinic/vet/controller/VetController.java
+++ monolith/src/main/java/org/springframework/samples/petclinic/vet/controller/VetController.java
@@ -15,7 +15,6 @@
  */
 package org.springframework.samples.petclinic.vet.controller;
 
-import org.springframework.samples.petclinic.vet.service.VetService;
 import org.springframework.samples.petclinic.vet.service.VetServiceInterface;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
